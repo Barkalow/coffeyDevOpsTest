57f7541b461fc5bad39074e7346a0a334f3e0c5d
ACoffey <coffeyDev@dev.com>
2023-07-28 13:17:06 -0500

    JDK

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,17 +1,5 @@
-# echo "Installing JDK"
-# sudo apt-get install openjdk-8-jdk
-
-# Install OpenJDK-8
-RUN apt-get update && \
-    apt-get install -y openjdk-8-jdk && \
-    apt-get install -y ant && \
-    apt-get clean;
-    
-# Fix certificate issues
-RUN apt-get update && \
-    apt-get install ca-certificates-java && \
-    apt-get clean && \
-    update-ca-certificates -f;
+echo "Installing JDK"
+sudo apt-get install openjdk-8-jdk
 
 # Setup JAVA_HOME -- useful for docker commandline
 ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
