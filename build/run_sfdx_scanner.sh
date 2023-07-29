58b2a91f2876a6c5aaaad8c430a0751f85a08939
ACoffey <coffeyDev@dev.com>
2023-07-29 12:33:05 -0500

    JDK

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,8 +1,5 @@
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >>~/.bashrc
-echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
-
 echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format=junit -o "sfdxScannerAnalysis.xml" -s 1
