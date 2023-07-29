1b28bd5bc15d7974165316424035abd6d3907cb6
ACoffey <coffeyDev@dev.com>
2023-07-29 10:40:31 -0500

    JDK

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -2,5 +2,3 @@
 
 echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
-echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >>~/.bashrc
-echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,5 +1,8 @@
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
 
+echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >>~/.bashrc
+echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
+
 echo "Running SFDX Scanner"
 npx sfdx scanner:run --target "**/default/**" --format=junit -o "sfdxScannerAnalysis.xml" -s 1
